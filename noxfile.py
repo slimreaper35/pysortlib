b3f450163ac2dcd39a7b9469f8aa3add2e9ac07d
Michal Šoltis <msoltis@redhat.com>
2025-01-02 12:53:15 +0100

    Bump python version
    Signed-off-by: Michal Šoltis <msoltis@redhat.com>

--- noxfile.py
+++ noxfile.py
@@ -4,7 +4,7 @@ from nox.sessions import Session
 nox.options.reuse_venv = "always"
 
 
-@nox.session(python="3.12")
+@nox.session(python="3.13")
 def ruff(session: Session) -> None:
     """Run ruff to perform static code analysis."""
     deps = ["ruff"]
@@ -12,7 +12,7 @@ def ruff(session: Session) -> None:
     session.run("ruff", "check", "pysortlib", "tests", "noxfile.py", silent=True)
 
 
-@nox.session(python="3.12")
+@nox.session(python="3.13")
 def mypy(session: Session) -> None:
     """Run mypy to perform static type checking."""
     deps = ["mypy"]
@@ -20,7 +20,7 @@ def mypy(session: Session) -> None:
     session.run("mypy", "pysortlib", "tests", "noxfile.py", silent=True)
 
 
-@nox.session(python="3.12")
+@nox.session(python="3.13")
 def pytest(session: Session) -> None:
     """Run pytest to execute unit tests."""
     deps = ["hypothesis", "pytest", "pytest-cov", "pytest-rich"]
